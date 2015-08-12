a111d2c3f15593ce591f1bae37c84f76687983b6
root <root@ryloth.mind-trick.net>
2015-08-12 10:10:07 -0400

    Renamed to support simple/compound attribute sets

---
+++ src/main/java/org/darkware/hero/people/SimpleAttributes.java
@@ -0,0 +1,15 @@
+package org.darkware.hero.people;
+
+import org.darkware.hero.base.EnumValueSet;
+
+/**
+ * @author jeff
+ * @since 2015-08-07
+ */
+public class SimpleAttributes extends EnumValueSet<Attribute>
+{
+    public SimpleAttributes()
+    {
+        super(0, Attribute.class);
+    }
+}
