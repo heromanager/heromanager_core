8973e179de9638f9d8cbf7e7947ca719bfe9f1e0
heromanager <heromanager@darkware.org>
2015-09-13 18:54:29 -0400

    Added an ItemSlot enum to track where items can be equipped.

---
+++ src/main/java/org/darkware/hero/item/ItemSlot.java
@@ -0,0 +1,25 @@
+package org.darkware.hero.item;
+
+/**
+ * @author jeff
+ * @since 2015-09-10
+ */
+public enum ItemSlot
+{
+    HEAD,
+    NECK,
+
+    CHEST,
+    ARMS,
+    HANDS,
+    FINGER,
+
+    WAIST,
+    LEGS,
+    FEET,
+
+    WEAPON,
+    SHIELD,
+
+    NO_EQUIP
+}
