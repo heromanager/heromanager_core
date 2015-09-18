548892b7d1afb4009dbfdd4ddba75c6cb79c8abd
heromanager <heromanager@darkware.org>
2015-09-17 22:56:15 -0400

    Added the base implementation.

---
+++ src/main/java/org/darkware/hero/people/mutation/Mutation.java
@@ -0,0 +1,16 @@
+package org.darkware.hero.people.mutation;
+
+import org.darkware.hero.base.NamedStaticObject;
+import org.darkware.hero.base.StaticId;
+
+/**
+ * @author jeff
+ * @since 2015-09-06
+ */
+public class Mutation extends NamedStaticObject
+{
+    public Mutation(final StaticId id, final CharSequence name)
+    {
+        super(id, name);
+    }
+}
